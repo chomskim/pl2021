406543d4efad2e75a40779ca0fb3fd46cf399376
chomskim <csknova@gmail.com>
2021-05-17 14:58:52 +0900

    update poker trans

--- 1-project-5/poker-transform/pokerTransform.js
+++ 1-project-5/poker-transform/pokerTransform.js
@@ -173,11 +173,11 @@ function printFiveCardsList(fcList) {
 }
 printFiveCardsList(cardList)
 
-function getPairs(list) {
-  if (list.length < 2) return []
+function getPairs(fcList) {
+  if (fcList.length < 2) return []
   else {
-    if (list[0] == list[1]) return [list[0]].concat(getPairs(list.slice(2, list.length)))
-    else return getPairs(list.slice(1, list.length))
+    if (fcList[0] === fcList[1]) return [fcList[0]].concat(getPairs(fcList.slice(2, fcList.length)))
+    else return getPairs(fcList.slice(1, fcList.length))
   }
 }
 
@@ -185,5 +185,38 @@ let rankList = cardList.map((fc) =>
   fc.fiveCards.map((ca) => (ca.rank === Card.ACE ? Card.KING + 1 : ca.rank))
 )
 console.log(`rankList=${JSON.stringify(rankList)}`)
+
 let pairList = rankList.map(getPairs)
 console.log(`pairList=${JSON.stringify(pairList)}`)
+
+const isStraight = (list5) =>
+  list5.reduce(
+    (prev, curr, i) => (i === list5.length - 1 ? prev : prev && list5[i] === list5[i + 1] + 1),
+    true
+  )
+
+function changeAce(fcList) {
+  let resList = [...fcList]
+  let aceIndex = resList.indexOf(14) // has Ace
+  if (aceIndex !== -1) {
+    resList[aceIndex] = 1
+    resList.sort((x, y) => y - x)
+  }
+  return resList
+}
+function getStraightValueTop(fcList) {
+  // return 0 if not straight
+  // return fcList[0] if straight
+  // return newList[0] if has Ace and changed to 1 is straight
+  let straight = isStraight(fcList)
+  if (straight) return fcList[0]
+
+  let aceIndex = fcList.indexOf(14) // has Ace
+  if (aceIndex !== -1) {
+    let newList = changeAce(fcList)
+    if (isStraight(newList)) return newList[0]
+  }
+  return 0
+}
+let straightList = rankList.map(getStraightValueTop)
+console.log(`straightList=${JSON.stringify(straightList)}`)
