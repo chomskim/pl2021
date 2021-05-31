d138cb49325f8aef20f6fbc1d22f16ab02166178
chomskim <csknova@gmail.com>
2021-05-31 12:50:18 +0900

    update poker web

--- 3-web-card/1-web-5-cards-layout/js/app.js
+++ 3-web-card/1-web-5-cards-layout/js/app.js
@@ -1,8 +1,8 @@
 // sort btn
 const sortBtn = document.querySelector('.sort-btn')
 let isSort = true
-console.log(JSON.stringify(sortBtn))
 sortBtn.addEventListener('click', function () {
+  console.log(sortBtn)
   console.log(`${sortBtn.textContent}  clicked`)
   sortBtn.textContent = isSort ? 'retry' : 'sort'
   isSort = !isSort
