3996e375087c66a031d9c6532b36f6c32622ff4a
chomskim <csknova@gmail.com>
2021-05-31 10:45:48 +0900

    add  poker sort

--- 3-web-card/3-web-5-cards-sort/js/app.js
+++ 3-web-card/3-web-5-cards-sort/js/app.js
@@ -1,18 +1,32 @@
 import Card from './card.js'
+import FiveCardsComparator from './fivecards-comparator.js'
 import initPokerList from './initpokerList.js'
+import quickSort from './quickSort.js'
 
 // DOM of grid-container
-const fiveCardsList = initPokerList()
+let fiveCardsList = initPokerList()
 generatePokerImageHTML(fiveCardsList)
 // sort btn
 const sortBtn = document.querySelector('.sort-btn')
 let isSort = true
 sortBtn.addEventListener('click', function () {
-  console.log(JSON.stringify(sortBtn))
-  console.log(`${sortBtn.textContent}  clicked`)
+  if (isSort) {
+    doSortAction()
+  } else {
+    doRetryAction()
+  }
   sortBtn.textContent = isSort ? 'retry' : 'sort'
   isSort = !isSort
 })
+function doSortAction() {
+  const comp = new FiveCardsComparator()
+  const sortedFiveCardsList = quickSort(fiveCardsList, comp.compare)
+  generatePokerImageHTML(sortedFiveCardsList)
+}
+function doRetryAction() {
+  fiveCardsList = initPokerList()
+  generatePokerImageHTML(fiveCardsList)
+}
 function getCName(card) {
   let cname
   switch (card.suit) {
@@ -49,6 +63,7 @@ function getCName(card) {
 }
 function generatePokerImageHTML(fiveCardsList) {
   const pokerImageList = document.querySelector('.grid-container')
+  pokerImageList.innerHTML = ''
   fiveCardsList.forEach((fc) => {
     const cards = fc.fiveCards
     let inners = cards.map((ca) => {
